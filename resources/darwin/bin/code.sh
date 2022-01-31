596cee83249e0b9f06824451f716e41c9747d248
deepak1556 <hop2deep@gmail.com>
2022-01-31 18:45:52 +0900

    refactor: use mdfind as a reliable way to determine path

--- resources/darwin/bin/code.sh
+++ resources/darwin/bin/code.sh
@@ -3,16 +3,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-function realpath() {
-	SOURCE=$1
-	while [ -h "$SOURCE" ]; do
-  		DIR=$( dirname "$SOURCE" )
-  		SOURCE=$(readlink "$SOURCE")
-  		[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
-	done
-	echo "$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+function app_path() {
+	BUNDLE_IDENTIFIER=""
+	if [[ $1 == *"/code-insiders" ]]; then
+		BUNDLE_IDENTIFIER="com.microsoft.VSCodeInsiders"
+	elif [[ $1 == *"/code-exploration" ]]; then
+		BUNDLE_IDENTIFIER="com.microsoft.VSCodeExploration"
+	else
+		BUNDLE_IDENTIFIER="com.microsoft.VSCode"
+	fi
+	echo "$(mdfind kMDItemCFBundleIdentifier = "${BUNDLE_IDENTIFIER}")"
 }
-CONTENTS="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
+CONTENTS="$(app_path "${BASH_SOURCE[0]}")/Contents"
 ELECTRON="$CONTENTS/MacOS/Electron"
 CLI="$CONTENTS/Resources/app/out/cli.js"
 ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" --ms-enable-electron-run-as-node "$@"
